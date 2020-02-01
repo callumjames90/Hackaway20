0e9f110c9670c090d61155319f71e6d8a3c919bd
gavestonhall <Sam.Burchell.2018@live.rhul.ac.uk>
2020-02-01 19:35:22 +0000

    Added mongo test seeder and auth

---
+++ database/seeds/TestMongo.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TestMongo extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'hunter2',
+                'email' => 'hunter2@hunter2.com',
+                'password' => Hash::make('hunter2'),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
