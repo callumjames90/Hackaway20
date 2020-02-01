4ff672c84cd13d5b1c86ad10da4b29d07e296a20
gavestonhall <Sam.Burchell.2018@live.rhul.ac.uk>
2020-02-01 18:31:58 +0000

    Setup mongodb atlas

--- config/database.php
+++ config/database.php
@@ -43,6 +43,23 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGO_DB_HOST', 'localhost'),
+            'port'     => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE'),
+            'username' => env('MONGO_DB_USERNAME'),
+            'password' => env('MONGO_DB_PASSWORD'),
+            'options'  => []
+        ],
+
+        'mongodb_conn' => [
+            'driver' => 'mongodb',
+            'dsn'=> 'mongodb+srv://admin:notpassword@cluster0-sqqnz.gcp.mongodb.net/test?retryWrites=true&w=majority',
+            'database' => 'my_data',
+        ],
+
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
