167970f04b9280351476807a4f92a7cd3b3f3318
Bruce Lay <33815726+brucelay@users.noreply.github.com>
2020-02-01 23:02:59 +0000

    Created RadarAPI object in JavaScript for interacting with the Radar API

---
+++ resources/js/radar.js
@@ -0,0 +1,14 @@
+function RadarAPI(api_key, radius) {
+    this.api_key = api_key;
+    this.radius = radius;
+    this.api_root_endpoint = "https://api.radar.io/v1/"
+
+    function createReviewGeofence(longitude, latitude, username) {
+    }
+
+    function getTouchingGeofences(longitude, latitude) {
+    }
+
+    function createGeofence(longitude, latitude) {
+    }
+}
