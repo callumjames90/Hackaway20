1f928a4404472099d2acb9f1316b0b5d43f31a13
Bruce Lay <33815726+brucelay@users.noreply.github.com>
2020-02-01 23:28:52 +0000

    Added function to list geofences to the RadarAPI class

--- resources/js/radar.js
+++ resources/js/radar.js
@@ -1,14 +1,33 @@
 function RadarAPI(api_key, radius) {
     this.api_key = api_key;
     this.radius = radius;
-    this.api_root_endpoint = "https://api.radar.io/v1/"
+    this.api_root_endpoint = "https://api.radar.io/v1/";
+    this.api_geofences_endpoint = this.api_root_endpoint + "geofences";
+    this.xhttp = new XMLHttpRequest();
 
-    function createReviewGeofence(longitude, latitude, username) {
+    this.createReviewGeofence = function (longitude, latitude, username) {
     }
 
-    function getTouchingGeofences(longitude, latitude) {
+    this.getTouchingGeofences = function (longitude, latitude) {
     }
 
-    function createGeofence(longitude, latitude) {
+    this.createGeofence = function (longitude, latitude) {
+    }
+
+    this.getGeofences = function() {
+        const jsonData = this.httpRequestJSON(this.xhttp, this.api_geofences_endpoint);
+        console.log(jsonData);
+    }
+
+    this.httpRequestJSON = function(xhttp, url) {
+        const HTTP_OK = 200;
+        const DONE_STATE = 4;
+
+        xhttp.onreadystatechange = function() {
+            if(this.readyState == DONE_STATE && this.status == HTTP_OK) {
+                const jsonData = JSON.parse(this.responseText);
+                return jsonData;
+            }
+        }
     }
 }
