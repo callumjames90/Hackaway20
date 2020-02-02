bf0791b3f2d1b28d305c218d3c7c83da5a2a5f47
Bruce Lay <33815726+brucelay@users.noreply.github.com>
2020-02-02 05:26:43 +0000

    Added main map

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,67 @@
 @extends('layouts.navbar')
 
 @section('content')
+
+<div id="map" class="container-fluid" style="height: 80%">
+    <script>
+        var map;
+
+        function initMap() {
+            var center = {lat: 51.4, lng: -0.56};
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: center,
+                zoom: 15,
+                draggable: true,
+                zoomControl: true,
+                mapTypeControl: false,
+                scaleControl: false,
+                rotateControl: false,
+                fullscreenControl: true,
+                streetViewControl: false,
+            });
+            var marker = new google.maps.Marker({position: center, map: map});
+
+            if (navigator.geolocation) {
+                <!--
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    map.setCenter(pos);-->
+
+                var heatmapData = [
+                    new google.maps.LatLng(37.782, -122.447),
+                    new google.maps.LatLng(37.782, -122.445),
+                    new google.maps.LatLng(37.782, -122.443),
+                    new google.maps.LatLng(37.782, -122.441),
+                    new google.maps.LatLng(37.782, -122.439),
+                    new google.maps.LatLng(37.782, -122.437),
+                    new google.maps.LatLng(37.782, -122.435),
+                    new google.maps.LatLng(37.785, -122.447),
+                    new google.maps.LatLng(37.785, -122.445),
+                    new google.maps.LatLng(37.785, -122.443),
+                    new google.maps.LatLng(37.785, -122.441),
+                    new google.maps.LatLng(37.785, -122.439),
+                    new google.maps.LatLng(37.785, -122.437),
+                    new google.maps.LatLng(37.785, -122.435)
+                ];
+
+                heatmap = new google.maps.visualisation.HeatmapLayer({
+                    data: heatmapData
+                });
+                heatmap.setMap(map)
+            }
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPhnYv7qqmn9NK7IweTP07rggklVMCc2U&callback=initMap" async defer></script>
+</div>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
         </div>
     </div>
 </div>
