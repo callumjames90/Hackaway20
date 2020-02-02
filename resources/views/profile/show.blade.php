d266efc52408f4ac50a245b4c45cd6619389c99e
callumjames90 <callum.clapton.2018@live.rhul.ac.uk>
2020-02-02 12:11:15 +0000

    Semi-finalised profile page displaying user info and previous reviews.

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -4,15 +4,23 @@
 
 @section('content')
     <div class="container">
+        <div class="icon-header">
+            <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+        </div>
 
-        @foreach($reviews as $review)
+        <h2 class="info-display">{{ Auth::user()->name }}</h2>
+        <h5 class="info-display">{{ Auth::user()->email }}</h5>
+
+        @foreach(Auth::user()->reviews as $review)
             @if (Auth::user()->_id === $review->user_id)
-                <div class="card mb-3">
-                    <h5 class="card-header">Review ID: {{ $review->id }}</h5>
+                <div class="card mb-3 border-dark">
+                    <h5 class="card-header">Review {{ count(Auth::user()->reviews) - $loop->index }} - Rated: {{ $review->rating }}</h5>
                     <div class="card-body">
                         <h5 class="card-title">Description</h5>
                         <p class="card-text">{{ $review->details }}</p>
                     </div>
+                    <div class="card-footer rev">Reviewed: {{ $review->created_at }}</div>
+                    <div class="card-footer">ID: {{ $review->id }}</div>
                 </div>
             @endif
         @endforeach
