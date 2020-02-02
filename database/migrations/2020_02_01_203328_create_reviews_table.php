8afbc0bd28910993a0efe3313647300f71d70fb0
henit3 <henit33@gmail.com>
2020-02-02 03:06:58 +0000

    Seeder changed to work with the original co-ordinates from radar and the new google api.

--- database/migrations/2020_02_01_203328_create_reviews_table.php
+++ database/migrations/2020_02_01_203328_create_reviews_table.php
@@ -15,15 +15,27 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('radar_id');
             $table->text('details')->nullable();
             $table->text('details')->nullable();
             $table->tinyInteger('rating');
+            $table->double('latitude');
+            $table->double('longitude');
             $table->integer('cluster_id')->references('id')->on('cluster');
             $table->integer('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
+
+    /*
+ *  CREATE TABLE `markers` (
+      `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+      `name` VARCHAR( 60 ) NOT NULL ,
+      `address` VARCHAR( 80 ) NOT NULL ,
+      `lat` FLOAT( 10, 6 ) NOT NULL ,
+      `lng` FLOAT( 10, 6 ) NOT NULL ,
+      `type` VARCHAR( 30 ) NOT NULL
+    ) ENGINE = MYISAM ;
+ */
 
     /**
      * Reverse the migrations.
