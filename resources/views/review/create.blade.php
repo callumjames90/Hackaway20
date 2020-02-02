479bdfed42f41387ce42ed8dd3e6fcc441a21040
Bruce Lay <33815726+brucelay@users.noreply.github.com>
2020-02-02 04:27:25 +0000

    please

--- resources/views/review/create.blade.php
+++ resources/views/review/create.blade.php
@@ -23,26 +23,20 @@
                     draggable: false,
                 });
                 var marker = new google.maps.Marker({position: center, map: map});
+
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        var pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        map.setCenter(pos);
+                    });
+                }
             }
         </script>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPhnYv7qqmn9NK7IweTP07rggklVMCc2U&callback=initMap" async defer></script>
     </div>
-
-    <script>
-        function getPosition(position, errorHandler) {
-            userLatitude = position.coords.latitude;
-            userLongitude = position.coords.longitude;
-            latlng = google.maps.LatLng(userLatitude, userLongitude);
-            document.getElementById("map").setCenter(latlng);
-        }
-
-        function setCurrentLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(getPosition);
-            }
-        }
-    </script>
-    <button onclick="setCurrentLocation()">Set Location</button>
 
     <div class="container">
         <div class = "row justify-content-center">
